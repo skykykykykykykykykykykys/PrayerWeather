a5baedd180400e3e5d3b338cd7c7777c8dc0d9ba
Ismail F Aziz <25928284+skykykykykykykykykykykys@users.noreply.github.com>
2019-12-02 21:06:19 +0700

    done basic html

--- main.py
+++ main.py
@@ -3,10 +3,29 @@ import requests
 
 app = Flask(__name__)
 
-@app.route('/weather', methods=['POST'])
-def weather():
-     weather_cond = ""
-     return render_template('weather.html', weather = weather_cond)
+@app.route('/info', methods=['POST'])
+def info():
+     city = request.form['cityname']
+     #country_code = request.form['country']
+     r_weather = requests.get('http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=9770ca72a55a683022867e21dfd84f9e')
+     json_weather = r_weather.json()
+     #temperature
+     temp_k = float(json_weather['main']['temp'])
+     temp_c = (temp_k - 273.15)
+     #weather
+     weather_status = str(json_weather["weather"][0]["main"])
+     #Prayer
+     r_pray = requests.get('http://muslimsalat.com/'+city+'.json')
+     json_pray = r_pray.json()
+     fajr = json_pray["items"][0]["fajr"]
+     dzuhur = json_pray["items"][0]["dhuhr"]
+     asr = json_pray["items"][0]["asr"]
+     maghrib = json_pray["items"][0]["maghrib"]
+     isha = json_pray["items"][0]["isha"]
+
+     prayer_time = json_pray["items"]
+
+     return render_template('result.html', temp=round(temp_c,2), weather=weather_status, prayer_time = prayer_time, city=city, fajr=fajr, dzuhur=dzuhur, asr=asr, maghrib=maghrib, isha=isha)
 
 @app.route('/')
 def index():
